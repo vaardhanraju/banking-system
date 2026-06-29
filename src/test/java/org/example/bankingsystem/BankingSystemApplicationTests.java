7eccbb8ef2823da922c7fb14b4dd666b5210b298
vaardhanraju <vaardhanraju@gmail.com>
2026-06-29 17:11:31 +0000

    Initial commit

---
+++ src/test/java/org/example/bankingsystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
