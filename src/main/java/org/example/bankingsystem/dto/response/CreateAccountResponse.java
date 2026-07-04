ad10d28c193949eaaa1b73f754136ee02bf78ddd
vaardhanraju <vaardhanraju@gmail.com>
2026-07-04 10:25:53 +0000

    refactor: add more details CreateAccountResponse

--- src/main/java/org/example/bankingsystem/dto/response/CreateAccountResponse.java
+++ src/main/java/org/example/bankingsystem/dto/response/CreateAccountResponse.java
@@ -3,11 +3,18 @@ package org.example.bankingsystem.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.bankingsystem.enums.AccountStatus;
+import org.example.bankingsystem.enums.AccountType;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateAccountResponse {
-    private String status;
-    private String message;
+    private String accountNumber;
+    private Double balance;
+    private AccountType accountType;
+    private AccountStatus accountStatus;
+    private LocalDate dateCreated;
 }
